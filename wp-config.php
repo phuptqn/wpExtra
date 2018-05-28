3116c4e585971aee4036a78b23b40853752d9140
Phan Thanh Phú <phu@bonseyjaden.com>
2018-05-28 14:53:43 +0700

    Update

--- wp-config.php
+++ wp-config.php
@@ -37,6 +37,9 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+define('WP_POST_REVISIONS', 4);
+define('FS_METHOD', 'direct');
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
